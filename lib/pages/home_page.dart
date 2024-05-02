0506aa6761ce93ba35a82c3320bb48acc660cd1e
AndresCanto <62972472+AndresCanto@users.noreply.github.com>
2024-05-02 01:33:33 -0600

    move content from home page to projects page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,119 +1,12 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:notes_app/services/firestore.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  // firestore
-  final FirestoreService firestoreService = FirestoreService();
-
-  // text controller
-  final TextEditingController textController = TextEditingController();
-
-  // open a dialog box to add a note
-  void openNoteBox({String? docID}) {
-    showDialog(
-      context: context,
-      builder: (context) => AlertDialog(
-        content: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            const Text("Ingresa el nombre del proyecto"),
-            TextField(controller: textController),
-            SizedBox(height: 50),
-            const Text("Ingresa una breve descripción"),
-            TextField(controller: textController),
-          ],
-        ),
-        actions: [
-          // button to save
-          ElevatedButton(
-              onPressed: () {
-                // add a new note
-                if (docID == null) {
-                  firestoreService.addNote(textController.text);
-                }
-                //update an existing note
-                else {
-                  firestoreService.updateNote(docID, textController.text);
-                }
-
-                // clear the text controller
-                textController.clear();
-
-                // close the box
-                Navigator.pop(context);
-              },
-              child: const Text('Add'))
-        ],
-      ),
-    );
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text("Notes")),
-      floatingActionButton: FloatingActionButton(
-        onPressed: openNoteBox,
-        child: const Icon(Icons.add),
-      ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: firestoreService.getNotesStream(),
-        builder: (context, snapshot) {
-          //  if we have data, get all the docs
-          if (snapshot.hasData) {
-            List notesList = snapshot.data!.docs;
 
-            //display as a list
-            return ListView.builder(
-              itemCount: notesList.length,
-              itemBuilder: (context, index) {
-                //get each individual doc
-                DocumentSnapshot document = notesList[index];
-                String docID = document.id;
-
-                //get note from each doc
-                Map<String, dynamic> data =
-                    document.data() as Map<String, dynamic>;
-                String noteText = data['note'];
-
-                //display as list title
-                return ListTile(
-                  title: Text(noteText),
-                  trailing: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      // update button
-                      IconButton(
-                        onPressed: () => openNoteBox(docID: docID),
-                        icon: const Icon(Icons.edit),
-                      ),
-
-                      // delete button
-                      IconButton(
-                        onPressed: () => firestoreService.deleteNote(docID),
-                        icon: const Icon(Icons.delete),
-                      ),
-                    ],
-                  ),
-                );
-              },
-            );
-          }
-
-          // if there is no data, return nothing
-          else {
-            return const Text("No notes...");
-          }
-        },
-      ),
     );
   }
 }
