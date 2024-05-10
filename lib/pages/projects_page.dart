9fc53b2dc808b775368f688568b44a2645d35ffa
AndresCanto <62972472+AndresCanto@users.noreply.github.com>
2024-05-09 21:34:24 -0600

    add safe area

--- lib/pages/projects_page.dart
+++ lib/pages/projects_page.dart
@@ -34,50 +34,52 @@ class _ProjectsPageState extends State<ProjectsPage> {
           child: const Icon(Icons.add, color: Colors.blue),
         ),
       ),
-      body: StreamBuilder<QuerySnapshot>(
-        stream: firestoreService.getProjectsStream(),
-        builder: (context, snapshot) {
-          //  if we have data, get all the docs
-          if (snapshot.hasData) {
-            List projectsList = snapshot.data!.docs;
-
-            //display as a list
-            return ListView.builder(
-              itemCount: projectsList.length,
-              itemBuilder: (context, index) {
-                //get each individual doc
-                DocumentSnapshot document = projectsList[index];
-                String docID = document.id;
-
-                //get note from each doc
-                Map<String, dynamic> data =
-                    document.data() as Map<String, dynamic>;
-                String projectTitle = data['title'];
-                String projectDescription = data['description'];
-
-                //display as list title
-                return ListTile(
-
-                  title: Text(
-                    projectTitle,
-                    style: const TextStyle(fontSize: 30),
-                  ),
-                  subtitle: Text(
-                    projectDescription,
-                    style: TextStyle(
-                        fontSize: 20,
-                        color: Theme.of(context).colorScheme.secondary),
-                  ),
-                );
-              },
-            );
-          }
-
-          // if there is no data, return nothing
-          else {
-            return const Text("No hay proyectos...");
-          }
-        },
+      body: SafeArea(
+        child: StreamBuilder<QuerySnapshot>(
+          stream: firestoreService.getProjectsStream(),
+          builder: (context, snapshot) {
+            //  if we have data, get all the docs
+            if (snapshot.hasData) {
+              List projectsList = snapshot.data!.docs;
+        
+              //display as a list
+              return ListView.builder(
+                itemCount: projectsList.length,
+                itemBuilder: (context, index) {
+                  //get each individual doc
+                  DocumentSnapshot document = projectsList[index];
+                  String docID = document.id;
+        
+                  //get note from each doc
+                  Map<String, dynamic> data =
+                      document.data() as Map<String, dynamic>;
+                  String projectTitle = data['title'];
+                  String projectDescription = data['description'];
+        
+                  //display as list title
+                  return ListTile(
+        
+                    title: Text(
+                      projectTitle,
+                      style: const TextStyle(fontSize: 30),
+                    ),
+                    subtitle: Text(
+                      projectDescription,
+                      style: TextStyle(
+                          fontSize: 20,
+                          color: Theme.of(context).colorScheme.secondary),
+                    ),
+                  );
+                },
+              );
+            }
+        
+            // if there is no data, return nothing
+            else {
+              return const Text("No hay proyectos...");
+            }
+          },
+        ),
       ),
     );
   }
